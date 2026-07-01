e0e4791438de61e351e5dd4115b2ff624c250d27
JaceOfSpades-Shambhala <jsj715@gmail.com>
2026-07-01 15:43:12 -0400

    Add editable camp location config

---
+++ camp-location.js
@@ -0,0 +1,15 @@
+/*
+  Update this file when Camp Hexadecibel has its festival location.
+
+  On a phone, edit only the latitude and longitude values below, then commit.
+  Example:
+    latitude: "49.123456",
+    longitude: "-117.123456"
+
+  If googleMapsUrl is filled in, it wins over latitude/longitude.
+*/
+window.CAMP_LOCATION = {
+  latitude: "",
+  longitude: "",
+  googleMapsUrl: ""
+};
